cf0a8c81ed9abab37d5d4e8cd327471fae09f72a
MohanadDarwish01 <mohanaddarwish01@gmail.com>
2025-04-20 14:47:54 +0200

    first commit

---
+++ src/api/social-media-request/routes/social-media-request.ts
@@ -0,0 +1,7 @@
+/**
+ * social-media-request router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::social-media-request.social-media-request');
